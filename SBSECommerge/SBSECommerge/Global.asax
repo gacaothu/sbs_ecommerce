ec75a69fa7ac962408f111eee26fe65c5b73d423
Hoai Nguyen <hoaint@tinhvan.com>
2017-03-17 09:49:19 +0700

    [Dev] upload source

---
+++ SBSECommerge/SBSECommerge/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SBSECommerge.MvcApplication" Language="C#" %>
---
+++ SBSECommerge/SBSECommerge/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SBSECommerge.Startup))]
+namespace SBSECommerge
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
