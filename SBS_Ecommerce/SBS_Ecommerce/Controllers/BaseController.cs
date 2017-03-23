abc0474b3f4164de2a0a3da18c11825571aab723
Hoai Nguyen <hoaint@tinhvan.com>
2017-03-23 10:27:17 +0700

    [Dev] - create Wishlist

--- SBS_Ecommerce/SBS_Ecommerce/Controllers/BaseController.cs
+++ SBS_Ecommerce/SBS_Ecommerce/Controllers/BaseController.cs
@@ -1,21 +1,22 @@
 ﻿using SBS_Ecommerce.Models;
 using SBS_Ecommerce.Models.Base;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
-using System.Web;
 using System.Web.Mvc;
 
 namespace SBS_Ecommerce.Controllers
 {
     public class BaseController : Controller
     {
+        private const string Tilde = "~";
+        private const string ThemeXmlPath = "/Content/theme.xml";
+
         public string GetLayout()
         {
             Helper helper = new Helper();
             List<Theme> themes = new List<Theme>();
-            themes = helper.DeSerialize(Server.MapPath("~") + "/Content/theme.xml");
+            themes = helper.DeSerialize(Server.MapPath(Tilde) + ThemeXmlPath);
             var layOut = themes.Where(m => m.Active == true).FirstOrDefault().Path;
             return layOut;
         }
@@ -38,7 +39,7 @@ namespace SBS_Ecommerce.Controllers
         {
             get
             {
-                return new AppUser(this.User as ClaimsPrincipal);
+                return new AppUser(User as ClaimsPrincipal);
             }
         }
 
@@ -46,7 +47,7 @@ namespace SBS_Ecommerce.Controllers
         {
             var thisUser = CurrentUser;
             SBS_DevEntities db = new SBS_DevEntities();
-            var user= db.Users.Where(m => m.Email == thisUser.Identity.Name).FirstOrDefault();
+            var user = db.Users.Where(m => m.Email == thisUser.Identity.Name).FirstOrDefault();
             return user.Id;
         }
     }
---
+++ SBS_Ecommerce/SBS_Ecommerce/Controllers/WishlistController.cs
@@ -0,0 +1,28 @@
+﻿using SBS_Ecommerce.Framework.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SBS_Ecommerce.Controllers
+{
+    public class WishlistController : BaseController
+    {
+        private const string ClassName = nameof(WishlistController);
+        private const string WishlistPath = "Wishlist/Wishlist.cshtml";
+
+        // GET: Wishlist
+        public ActionResult Wishlist()
+        {
+            string methodName = System.Reflection.MethodBase.GetCurrentMethod().Name;
+
+            LoggingUtil.StartLog(ClassName, methodName);
+
+            var viewPath = GetLayout() + WishlistPath;
+
+            LoggingUtil.EndLog(ClassName, methodName);
+            return View(viewPath);
+        }
+    }
+}
