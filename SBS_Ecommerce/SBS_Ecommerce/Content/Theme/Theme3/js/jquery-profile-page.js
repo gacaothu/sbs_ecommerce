4d2d3d6aa2b6fc476a1c9e61b03af58a298bd9aa
Trần Thị Thanh Hồng <hongttt@tinhvan.com>
2017-03-24 09:11:51 +0700

    Commit source code

---
+++ SBS_Ecommerce/SBS_Ecommerce/Content/Theme/Theme3/js/jquery-profile-page.js
@@ -0,0 +1,8 @@
+﻿$(".update-profile").click(function () {
+    $(".update-form").show();
+    $(".general-form").hide()
+})
+$(".update-shipping-address").click(function () {
+    $(".shipping-update").show();
+    $(".shipping-form").hide()
+})
