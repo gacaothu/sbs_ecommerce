12fc4d9067ccbf02f27d5ce9dd30af601f11a447
Hoai Nguyen <hoaint@tinhvan.com>
2017-03-28 09:36:01 +0700

    [Dev] - implement Search feature

--- SBS_Ecommerce/SBS_Ecommerce/Content/Theme/Theme3/js/cart.js
+++ SBS_Ecommerce/SBS_Ecommerce/Content/Theme/Theme3/js/cart.js
@@ -1,14 +1,24 @@
-﻿function addToCart(id,quantity) {
+﻿function addToCart(id, quantity) {
     $.ajax({
         url: "/Product/AddCart",
         data: { id: id, count: quantity },
         success: function (rs) {
-           window.location.href = "/Product/Checkout";
+            window.location.href = "/Product/Checkout";
         }
     });
 }
 
 function removeFromCart(id) {
+    $.ajax({
+        url: "/Product/RemoveCart",
+        data: { id: id },
+        success: function (rs) {
+            window.location.reload();
+        }
+    });
+}
+
+function removeCartHome(id) {
     $.ajax({
         url: "/Product/RemoveCart",
         data: { id: id },
