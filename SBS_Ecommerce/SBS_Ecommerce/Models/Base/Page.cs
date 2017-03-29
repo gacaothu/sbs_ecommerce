1baff266792104b59382f802cf7f93703821e2c3
MT420091511\PC <notvalue@gmail.com>
2017-03-29 09:06:49 +0700

    Comit source

---
+++ SBS_Ecommerce/SBS_Ecommerce/Models/Base/Page.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SBS_Ecommerce.Models.Base
+{
+    public class Page
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Content { get; set; }
+    }
+}
