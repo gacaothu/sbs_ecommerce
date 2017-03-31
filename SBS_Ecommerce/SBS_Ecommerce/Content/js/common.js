baa4285112c56918ee2f298b8ab6abc36b2e54c6
Hoai Nguyen <hoaint@tinhvan.com>
2017-03-31 10:30:57 +0700

    [Dev] - implement Search feature

--- SBS_Ecommerce/SBS_Ecommerce/Content/js/common.js
+++ SBS_Ecommerce/SBS_Ecommerce/Content/js/common.js
@@ -31,3 +31,71 @@ $(function () {
         }
     });
 });
+
+function addToWishlist(id) {
+    $.ajax({
+        type: 'POST',
+        url: '/Wishlist/InsertToWishlist',
+        data: {
+            id: id
+        },
+        success: function (rs) {
+            if (rs.reponse == 0) {
+                console.log("success");
+            } else if (rs.reponse == 1) {
+                console.log("existed");
+            } else if (rs.reponse == -1) {
+                console.log("failed");
+            } else {
+                alert("Please login first.");
+            }
+        }
+    });
+}
+
+function removeFromWishlist(id) {
+    $.ajax({
+        type: 'POST',
+        url: '/Wishlist/RemoveFromWishlist',
+        data: { id: id },
+        success: function (rs) {
+            if (rs.reponse == 0) {
+                location.reload();
+            } else if (rs.reponse == -1) {
+                console.log("failed");
+            } else {
+                alert("Please login first.");
+            }
+        }
+    });
+}
+
+function addToCart(id, quantity) {
+    $.ajax({
+        url: "/Product/AddCart",
+        data: { id: id, count: quantity },
+        success: function (rs) {
+            window.location.href = "/Product/Checkout";
+        }
+    });
+}
+
+function removeFromCart(id) {
+    $.ajax({
+        url: "/Product/RemoveCart",
+        data: { id: id },
+        success: function (rs) {
+            window.location.reload();
+        }
+    });
+}
+
+function removeCartHome(id) {
+    $.ajax({
+        url: "/Product/RemoveCart",
+        data: { id: id },
+        success: function (rs) {
+            window.location.reload();
+        }
+    });
+}
