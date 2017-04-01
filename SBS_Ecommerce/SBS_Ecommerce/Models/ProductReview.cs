1cea2e564ac0eae988d6446cf913dafbf3d5dddf
Hoai Nguyen <hoaint@tinhvan.com>
2017-04-01 08:59:36 +0700

    [Dev] - update Models

--- SBS_Ecommerce/SBS_Ecommerce/Models/ProductReview.cs
+++ SBS_Ecommerce/SBS_Ecommerce/Models/ProductReview.cs
@@ -15,7 +15,7 @@ namespace SBS_Ecommerce.Models
     public partial class ProductReview
     {
         public int Id { get; set; }
-        public int UId { get; set; }
+        public Nullable<int> UId { get; set; }
         public int ProId { get; set; }
         public string Title { get; set; }
         public string Content { get; set; }
