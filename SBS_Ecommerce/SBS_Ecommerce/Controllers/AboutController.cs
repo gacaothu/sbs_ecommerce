15b3df11e0ea2a980fd7bb54dccc8b0e7631d673
MT420091511\PC <notvalue@gmail.com>
2017-04-03 09:37:09 +0700

    Commit source

---
+++ SBS_Ecommerce/SBS_Ecommerce/Controllers/AboutController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SBS_Ecommerce.Controllers
+{
+    public class AboutController : BaseController
+    {
+        private const string AboutPath = "/About/Index.cshtml";
+        // GET: About
+        public ActionResult Index()
+        {
+            var pathView = GetLayout() + AboutPath;
+            return View(pathView);
+        }
+    }
+}
---
+++ SBS_Ecommerce/SBS_Ecommerce/Controllers/ContactController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SBS_Ecommerce.Controllers
+{
+    public class ContactController : BaseController
+    {
+        private const string ContactPath = "/Contact/Index.cshtml";
+        // GET: Contact
+        public ActionResult Index()
+        {
+            var pathView = GetLayout() + ContactPath;
+            return View(pathView);
+        }
+    }
+}
