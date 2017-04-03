43e06f6929a596cdc56c94a76e71400be1f8a246
MT420091511\PC <notvalue@gmail.com>
2017-04-03 13:58:37 +0700

    Commit source

--- SBS_Ecommerce/SBS_Ecommerce/Controllers/BlogController.cs
+++ SBS_Ecommerce/SBS_Ecommerce/Controllers/BlogController.cs
@@ -10,32 +10,33 @@ namespace SBS_Ecommerce.Controllers
         private const string PathTheme = "~/Views/Theme/";
         private const string BlogPath = "/Blog/Index.cshtml";
         private const string BlogDetailPath = "/Blog/Detail.cshtml";
+        private const int Count = 4;
         Helper helper = new Helper();
         private SBS_Entities db = new SBS_Entities();
 
         // GET: Blog
         public ActionResult Index(int ?page)
         {
-            int count = 4;
+            
             var pathView = GetLayout() + BlogPath;
-            var lstBlock = db.Blogs.Take(count).OrderByDescending(m=>m.UpdatedAt).ToList();
+            var lstBlock = db.Blogs.Take(Count).OrderByDescending(m=>m.UpdatedAt).ToList();
             int total = db.Blogs.Count();
             string showItem = "";
             int currentPage = 1;
             if (page != null)
             {
-                var pre = int.Parse(page.ToString()) * count - count;
-                var next = int.Parse(page.ToString()) * count;
+                var pre = int.Parse(page.ToString()) * Count - Count;
+                var next = int.Parse(page.ToString()) * Count;
                 lstBlock = db.Blogs.OrderByDescending(m=>m.UpdatedAt).Skip(pre).Take(next).ToList();
                 showItem = (pre + 1).ToString() + "-" + (next> total?total:next).ToString() ;
                 currentPage = int.Parse(page.ToString());
             }
             else
             {
-                showItem = "1-" + count.ToString();
+                showItem = "1-" + Count.ToString();
             }
 
-            ViewBag.NumberOfPage = (db.Blogs.Count() % count == 0 ? db.Blogs.Count() / count : db.Blogs.Count() / count + 1);
+            ViewBag.NumberOfPage = (db.Blogs.Count() % Count == 0 ? db.Blogs.Count() / Count : db.Blogs.Count() / Count + 1);
             ViewBag.Total = total;
             ViewBag.ShowItem = showItem;
             ViewBag.CurrentPage = currentPage;
@@ -46,6 +47,8 @@ namespace SBS_Ecommerce.Controllers
         {
             var blog = db.Blogs.Where(m => m.BlogId == id).FirstOrDefault();
             var pathView = GetLayout() + BlogDetailPath;
+            ViewBag.RecentBlog = db.Blogs.Take(Count).OrderByDescending(m => m.UpdatedAt).ToList();
+            ViewBag.Comment = db.BlogComments.Where(m => m.BlogId == id);
             return View(pathView, blog);
         }
 
