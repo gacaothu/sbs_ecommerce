47e281bbc74c75b17b4548beb4b2b96ca61cc40b
MT420091511\PC <notvalue@gmail.com>
2017-04-04 16:04:38 +0700

    Commit

--- SBS_Ecommerce/SBS_Ecommerce/Content/Admin/js/blogmanager.js
+++ SBS_Ecommerce/SBS_Ecommerce/Content/Admin/js/blogmanager.js
@@ -17,35 +17,7 @@ function ConfirmAddBlog() {
     $('#addBlogModal').modal('show');
 }
 
-function ConfirmEditBlock(id) {
-    $('#addBlogModal').find('h3').text('Edit Blog');
-    $('#addBlogModal').attr('data-action', 'Edit');
-    $('#addBlogModal').attr('data-id', id);
-    $.ajax({
-        url: UrlContent('/Admin/GetContentBlog'),
-        data: { id: id },
-        type: 'POST',
-        success: function (rs) {
-            CKEDITOR.instances['txtHTML'].setData(rs.Content);
-            if (rs.Thumb != "" && rs.Thumb != null) {
-                if ($('#imgThumbnail').parent().parent().find('img').attr('src') != undefined) {
-                    $('#imgThumbnail').parent().parent().find('img').attr('src', rs.Thumb);
-                }
-                else {
-                    $('#imgThumbnail').parent().parent().find('.imgSlider').empty().append('<img src="' + rs.Thumb + '" style="width:150px;height:100px;">');
-                }
-            }
-            else {
-                $('#imgThumbnail').val('');
-                $('#imgThumbnail').parent().parent().find('.imgSlider').empty();
-                $('#imgThumbnail').parent().parent().find('.imgSlider').append('<i class="glyphicon glyphicon-picture"></i><div style="padding-left:10px;padding-bottom: 13px;">No image</div>');
-            }
 
-            $('#addBlogModal').find('#txtTitleHTML').val(rs.Title);
-            $('#addBlogModal').modal('show');
-        }
-    });
-}
 
 function SaveBlog() {
     var content = CKEDITOR.instances['txtHTML'].getData();
@@ -96,7 +68,7 @@ function UploadThumbnail(type) {
         }
     }
     var xhr = new XMLHttpRequest();
-    xhr.open('POST', '/Admin/UploadImageThumbnail');
+    xhr.open('POST', UrlContent("/Admin/UploadImageThumbnail"));
     xhr.send(formdata);
     xhr.onreadystatechange = function () {
         if (xhr.readyState == 4 && xhr.status == 200) {
@@ -116,8 +88,7 @@ function UploadThumbnail(type) {
 
                 });
             }
-            else
-            {
+            else {
                 $.ajax({
                     url: UrlContent('/Admin/EditBlog'),
                     data: { id: $('#addBlogModal').attr('data-id'), content: content, title: $('#txtTitleHTML').val(), thumb: rs },
