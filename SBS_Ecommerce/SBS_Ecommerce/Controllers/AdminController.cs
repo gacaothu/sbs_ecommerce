9456ecc0e4a1e1e236ddf46a0908cdd9fa007952
MT420091511\PC <notvalue@gmail.com>
2017-04-11 16:45:37 +0700

    Update entity framework

--- SBS_Ecommerce/SBS_Ecommerce/Controllers/AdminController.cs
+++ SBS_Ecommerce/SBS_Ecommerce/Controllers/AdminController.cs
@@ -839,6 +839,11 @@ namespace SBS_Ecommerce.Controllers
             return View(lstMarketing);
         }
 
+        public ActionResult SendMailManager()
+        {
+            List<Marketing> lstMarketing = db.Marketings.ToList();
+            return View(lstMarketing);
+        }
         /// <summary>
         /// Create campaign
         /// </summary>
@@ -920,6 +925,18 @@ namespace SBS_Ecommerce.Controllers
             return Json(true, JsonRequestBehavior.AllowGet);
         }
 
+        public ActionResult GetEmailSystem()
+        {
+            var email = "";
+            foreach (var item in db.Users)
+            {
+                if (!string.IsNullOrEmpty(item.Email))
+                    email = email + " " + item.Email;
+            }
+
+            return Json(email, JsonRequestBehavior.AllowGet);
+        }
+
         /// <summary>
         /// Change status campaign
         /// </summary>
@@ -942,7 +959,7 @@ namespace SBS_Ecommerce.Controllers
             return Json(true, JsonRequestBehavior.AllowGet);
         }
 
-        public ActionResult SendMail(int id,string time, List<string> lstEmail,string subject)
+        public ActionResult SendMail(int id, string time, List<string> lstEmail, string subject)
         {
             try
             {
@@ -957,7 +974,7 @@ namespace SBS_Ecommerce.Controllers
                     datetime = DateTime.Now;
                 }
                 //DateTime datetime = new DateTime();
-                var emailMessage =  emailmarketing.Content;
+                var emailMessage = emailmarketing.Content;
                 this.SendEmail(subject, emailMessage, datetime, lstEmail);
             }
             catch
@@ -992,10 +1009,10 @@ namespace SBS_Ecommerce.Controllers
                 //To do
                 EmailUtil emailUT = new EmailUtil();
                 emailUT.SendListEmail(message);
-                
+
             });
         }
 
-       
+
     }
 }
--- SBS_Ecommerce/SBS_Ecommerce/Models/Theme.cs
+++ SBS_Ecommerce/SBS_Ecommerce/Models/Theme.cs
@@ -12,25 +12,26 @@ namespace SBS_Ecommerce.Models
     using System;
     using System.Collections.Generic;
     
-    public partial class Marketing
+    public partial class Theme
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
-        public Marketing()
+        public Theme()
         {
-            this.ScheduleEmails = new HashSet<ScheduleEmail>();
+            this.Layouts = new HashSet<Layout>();
+            this.Menus = new HashSet<Menu>();
+            this.Sliders = new HashSet<Slider>();
         }
     
         public int Id { get; set; }
-        public string NameCampain { get; set; }
-        public string ListEmail { get; set; }
-        public string Schedule { get; set; }
-        public string Title { get; set; }
-        public string Content { get; set; }
-        public Nullable<bool> Status { get; set; }
-        public Nullable<System.DateTime> CreatedAt { get; set; }
-        public Nullable<System.DateTime> UpdatedAt { get; set; }
+        public string Name { get; set; }
+        public string Path { get; set; }
+        public bool Active { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
-        public virtual ICollection<ScheduleEmail> ScheduleEmails { get; set; }
+        public virtual ICollection<Layout> Layouts { get; set; }
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Menu> Menus { get; set; }
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Slider> Sliders { get; set; }
     }
 }
