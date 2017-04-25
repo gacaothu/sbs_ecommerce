919d46bf38619bc16e300d38b617e9c817ef38f3
Hoai Nguyen <hoaint@tinhvan.com>
2017-04-25 11:20:06 +0700

    [Dev] - update Models

--- SBS_Ecommerce/SBS_Ecommerce/Controllers/AdminController.cs
+++ SBS_Ecommerce/SBS_Ecommerce/Controllers/AdminController.cs
@@ -23,6 +23,7 @@ using System.Security.Claims;
 using Microsoft.Owin.Security;
 using SBS_Ecommerce.Framework.Configurations;
 using System.Net;
+using SBS_Ecommerce.Models.Extension;
 
 namespace SBS_Ecommerce.Controllers
 {
@@ -1259,6 +1260,268 @@ namespace SBS_Ecommerce.Controllers
         }
         #endregion
 
+        private const string ClassName = nameof(AdminController);
+        private const string PathOrder = "~/Views/Admin/Orders.cshtml";
+        private const string PathPartialOrder = "~/Views/Admin/_PartialOrder.cshtml";
+        private const string PathPartialDetail = "~/Views/Admin/_PartialOrderDetail.cshtml";
+        private const string PathPartialPending = "~/Views/Admin/_PartialPendingOrders.cshtml";
+        private const string PathPartialProcessing = "~/Views/Admin/_PartialProcessingOrders.cshtml";
+        private const string PathPartialCompleted = "~/Views/Admin/_PartialCompletedOrders.cshtml";
+        private const string PathPartialCanceled = "~/Views/Admin/_PartialCanceledOrders.cshtml";
+        /// <summary>
+        /// Get Orders.
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Orders(int kind)
+        {
+            string methodName = System.Reflection.MethodBase.GetCurrentMethod().Name;
+            LoggingUtil.StartLog(ClassName, methodName);
 
+            try
+            {
+                //ViewBag.Count = db.Database.SqlQuery<int>(string.Format(CountQuery, kind)).Single();
+
+                switch (kind)
+                {
+                    case (int)OrderStatus.Pending:
+                        ViewBag.Data = GetOrders(OrderStatus.Pending);
+                        ViewBag.Partial = PartialViewToString(this, PathPartialPending, ViewBag.Data);
+                        break;
+                    case (int)OrderStatus.Processing:
+                        ViewBag.Data = GetOrders(OrderStatus.Processing);
+                        ViewBag.Partial = PartialViewToString(this, PathPartialProcessing, ViewBag.Data);
+                        break;
+                    case (int)OrderStatus.Completed:
+                        ViewBag.Data = GetOrders(OrderStatus.Completed);
+                        ViewBag.Partial = PartialViewToString(this, PathPartialCompleted, ViewBag.Data);
+                        break;
+                    case (int)OrderStatus.Cancelled:
+                        ViewBag.Data = GetOrders(OrderStatus.Completed);
+                        ViewBag.Partial = PartialViewToString(this, PathPartialCanceled, ViewBag.Data);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            catch (Exception e)
+            {
+                LoggingUtil.ShowErrorLog(ClassName, methodName, e.Message);
+            }
+
+            LoggingUtil.EndLog(ClassName, methodName);
+            return View(Url.Content(PathOrder));
+        }
+
+        /// <summary>
+        /// Get detail of Order.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <returns></returns>
+        public ActionResult OrderDetail(string id)
+        {
+            string methodName = System.Reflection.MethodBase.GetCurrentMethod().Name;
+            LoggingUtil.StartLog(ClassName, methodName);
+
+            List<OrderDetail> details = new List<OrderDetail>();
+            try
+            {
+                details = db.GetOrderDetails.Where(m => m.OrderId == id).ToList();
+            }
+            catch (Exception e)
+            {
+                LoggingUtil.ShowErrorLog(ClassName, methodName, e.Message);
+            }
+            ViewBag.Data = details;
+            LoggingUtil.EndLog(ClassName, methodName);
+            return Json(new { Partial = PartialViewToString(this, Url.Content(PathPartialDetail), ViewBag.Data) }, JsonRequestBehavior.AllowGet);
+        }
+
+        /// <summary>
+        /// Updates the shipping status.
+        /// </summary>
+        /// <param name="id">The order identifier.</param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult UpdateStatus(string id)
+        {
+            bool flag = false;
+            string methodName = System.Reflection.MethodBase.GetCurrentMethod().Name;
+            LoggingUtil.StartLog(ClassName, methodName);
+
+            try
+            {
+                var order = db.GetOrders.FirstOrDefault(c => c.OrderId == id);
+
+                switch (order.OrderStatus)
+                {
+                    case (int)OrderStatus.Pending:
+                        flag = true;
+                        order.OrderStatus = (int)OrderStatus.Processing;
+                        order.ShippingStatus = (int)ShippingStatus.NotYetShipped;
+                        break;
+                    case (int)OrderStatus.Processing:
+                        flag = true;
+                        order.OrderStatus = (int)OrderStatus.Completed;
+                        order.ShippingStatus = (int)ShippingStatus.Delivered;
+                        break;
+                    default:
+                        flag = false;
+                        break;
+                }
+
+                if (flag)
+                {
+                    order.UpdatedAt = DateTime.Now;
+                    var entry = db.Entry(order);
+                    entry.Property(m => m.OrderStatus).IsModified = true;
+                    entry.Property(m => m.ShippingStatus).IsModified = true;
+                    entry.Property(m => m.UpdatedAt).IsModified = true;
+                    db.SaveChanges();
+                    flag = true;
+                }
+            }
+            catch (Exception e)
+            {
+                LoggingUtil.ShowErrorLog(ClassName, methodName, e.Message);
+                flag = false;
+            }
+
+            LoggingUtil.EndLog(ClassName, methodName);
+            if (flag)
+            {
+                return Json(true, JsonRequestBehavior.AllowGet);
+            }
+            else
+                return Json(false, JsonRequestBehavior.AllowGet);
+        }
+
+        /// <summary>
+        /// Filters the order.
+        /// </summary>
+        /// <param name="status">The status.</param>
+        /// <param name="sortByDate">The sort by date.</param>
+        /// <param name="dateFrom">From date.</param>
+        /// <param name="dateTo">To date.</param>
+        /// <returns></returns>
+        public ActionResult FilterOrder(int kind, int? status, string sortByDate, string dateFrom, string dateTo)
+        {
+            string methodName = System.Reflection.MethodBase.GetCurrentMethod().Name;
+            LoggingUtil.StartLog(ClassName, methodName);
+
+            string partialString = "";
+            try
+            {
+                ViewBag.Data = ProcessFilter(kind, status, sortByDate, dateFrom, dateTo);
+                partialString = PartialViewToString(this, PathPartialOrder, ViewBag.Data);
+                ViewBag.Count = ViewBag.Data.Count;
+            }
+            catch (Exception e)
+            {
+                LoggingUtil.ShowErrorLog(ClassName, methodName, e.Message);
+            }
+
+            LoggingUtil.EndLog(ClassName, methodName);
+            return Json(new { Partial = partialString }, JsonRequestBehavior.AllowGet);
+        }
+
+        private List<Models.Order> ProcessFilter(int kind, int? status, string sort, string dateFrom, string dateTo, int offset = 0, int limit = 100)
+        {
+            string asc = "asc";
+            string desc = "desc";
+            List<Models.Order> result = new List<Models.Order>();
+            DateTime datefrom;
+            DateTime dateto;
+            if (kind == (int)OrderStatus.Processing)
+            {
+                if (sort == asc)
+                {
+                    result = db.GetOrders.Where(m => m.OrderStatus == kind && m.ShippingStatus == status).OrderBy(m => m.CreatedAt).Skip(offset).Take(limit).ToList();
+                }
+                else if (sort == desc)
+                {
+                    result = db.GetOrders.Where(m => m.OrderStatus == kind && m.ShippingStatus == status).OrderByDescending(m => m.CreatedAt).Skip(offset).Take(limit).ToList();
+                }
+                else
+                {
+                    if (!string.IsNullOrEmpty(dateFrom) && string.IsNullOrEmpty(dateTo))
+                    {
+                        datefrom = Convert.ToDateTime(dateFrom);
+                        result = db.GetOrders.Where(m => m.OrderStatus == kind && m.ShippingStatus == status && m.CreatedAt >= datefrom)
+                        .OrderByDescending(m => m.CreatedAt).Skip(offset).Take(limit).ToList();
+                    }
+                    else if (string.IsNullOrEmpty(dateFrom) && !string.IsNullOrEmpty(dateTo))
+                    {
+                        dateto = Convert.ToDateTime(dateTo);
+                        result = db.GetOrders.Where(m => m.OrderStatus == kind && m.ShippingStatus == status && m.CreatedAt <= dateto)
+                        .OrderByDescending(m => m.CreatedAt).Skip(offset).Take(limit).ToList();
+                    }
+                    else if (string.IsNullOrEmpty(dateFrom) && string.IsNullOrEmpty(dateTo))
+                    {
+                        result = db.GetOrders.Where(m => m.OrderStatus == kind && m.ShippingStatus == status)
+                        .OrderByDescending(m => m.CreatedAt).Skip(offset).Take(limit).ToList();
+                    }
+                    else
+                    {
+                        datefrom = Convert.ToDateTime(dateFrom);
+                        dateto = Convert.ToDateTime(dateTo);
+                        result = db.GetOrders.Where(m => m.OrderStatus == kind && m.ShippingStatus == status && m.CreatedAt >= datefrom && m.CreatedAt <= dateto)
+                        .OrderByDescending(m => m.CreatedAt).Skip(offset).Take(limit).ToList();
+                    }
+                }
+            }
+            else
+            {
+                if (sort == asc)
+                {
+                    result = db.GetOrders.Where(m => m.OrderStatus == kind).OrderBy(m => m.CreatedAt).Skip(offset).Take(limit).ToList();
+                }
+                else if (sort == desc)
+                {
+                    result = db.GetOrders.Where(m => m.OrderStatus == kind).OrderByDescending(m => m.CreatedAt).Skip(offset).Take(limit).ToList();
+                }
+                else
+                {
+
+                    if (!string.IsNullOrEmpty(dateFrom) && string.IsNullOrEmpty(dateTo))
+                    {
+                        datefrom = Convert.ToDateTime(dateFrom);
+                        result = db.GetOrders.Where(m => m.OrderStatus == kind && m.CreatedAt >= datefrom)
+                        .OrderByDescending(m => m.CreatedAt).Skip(offset).Take(limit).ToList();
+                    }
+                    else if (string.IsNullOrEmpty(dateFrom) && !string.IsNullOrEmpty(dateTo))
+                    {
+                        dateto = Convert.ToDateTime(dateTo);
+                        result = db.GetOrders.Where(m => m.OrderStatus == kind && m.CreatedAt <= dateto)
+                            .OrderByDescending(m => m.CreatedAt).Skip(offset).Take(limit).ToList();
+                    }
+                    else if (string.IsNullOrEmpty(dateFrom) && string.IsNullOrEmpty(dateTo))
+                    {
+                        result = db.GetOrders.Where(m => m.OrderStatus == kind).OrderByDescending(m => m.CreatedAt).Skip(offset).Take(limit).ToList();
+                    }
+                    else
+                    {
+                        datefrom = Convert.ToDateTime(dateFrom);
+                        dateto = Convert.ToDateTime(dateTo);
+                        result = db.GetOrders.Where(m => m.OrderStatus == kind && m.CreatedAt >= datefrom && m.CreatedAt <= dateto)
+                            .OrderByDescending(m => m.CreatedAt).Skip(offset).Take(limit).ToList();
+                    }
+                }
+            }
+            return result;
+        }
+
+        private List<Models.Order> GetOrders(OrderStatus kind)
+        {
+            List<Models.Order> result = new List<Models.Order>();
+            try
+            {
+                result = db.GetOrders.Where(m => m.OrderStatus == (int)kind).OrderBy(m => m.CreatedAt).ToList();
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            return result;
+        }
     }
 }
--- SBS_Ecommerce/SBS_Ecommerce/Models/SBS.Context.cs
+++ SBS_Ecommerce/SBS_Ecommerce/Models/SBS.Context.cs
@@ -56,8 +56,6 @@ namespace SBS_Ecommerce.Models
         public virtual DbSet<sysdiagram> sysdiagrams { get; set; }
         public virtual DbSet<Theme> Themes { get; set; }
         public virtual DbSet<Block> Blocks { get; set; }
-        public virtual DbSet<LocalPickup> LocalPickups { get; set; }
-        public virtual DbSet<WeightBased> WeightBaseds { get; set; }
         public virtual DbSet<Page> Pages { get; set; }
     
         public virtual int sp_alterdiagram(string diagramname, Nullable<int> owner_id, Nullable<int> version, byte[] definition)
