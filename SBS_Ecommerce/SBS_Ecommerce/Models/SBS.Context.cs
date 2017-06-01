32ef00262e91348373b195948fda7a0621682e2b
Toan <huutoannht@gmail.com>
2017-06-01 10:39:06 +0700

    Commit source code moidify time delivery

--- SBS_Ecommerce/SBS_Ecommerce/Models/SBS.Context.cs
+++ SBS_Ecommerce/SBS_Ecommerce/Models/SBS.Context.cs
@@ -63,6 +63,7 @@ namespace SBS_Ecommerce.Models
         public virtual DbSet<sysdiagram> sysdiagrams { get; set; }
         public virtual DbSet<ConfigMailChimp> ConfigMailChimps { get; set; }
         public virtual DbSet<SEO> SEOs { get; set; }
+        public virtual DbSet<ConfigHoliday> ConfigHolidays { get; set; }
     
         public virtual int sp_alterdiagram(string diagramname, Nullable<int> owner_id, Nullable<int> version, byte[] definition)
         {
