2ef715f88b66296d75ca920886021338adbb1f72
Hoai Nguyen <hoaint@tinhvan.com>
2017-06-17 15:05:22 +0700

    [Dev] - Update structure code

---
+++ SBS_Ecommerce/SBS_Ecommerce/Content/Admin/js/app-configuration-manager.js
@@ -0,0 +1,161 @@
+﻿var url = window.location.href;
+$(function () {
+    checkAlertMessageDisplay('.main-content');
+});
+function SaveConfig() {
+    msg_clear();
+
+    if ($("#txtPageID").val().trim() == "") {
+        $("#error-fbID").show();
+        return;
+    }
+    else {
+        $("#error-fbID").hide();
+    }
+    $.ajax({
+        url: '@Url.Action("SaveConfigChatting","Admin")',
+        data: { pageID: $('#txtPageID').val() },
+        success: function (rs) {
+            showAlertFromResponse(rs);
+        }
+    });
+}
+
+function SaveConfigMailChimp() {
+    if ($("#txtApiKey").val().trim() == "") {
+        $("#error-txtApiKey").show();
+        return;
+    }
+    else {
+        $("#error-txtApiKey").hide();
+    }
+
+    $.ajax({
+        url: '@Url.Action("SaveConfigMailChimp", "Admin")',
+        data: { apiKey: $('#txtApiKey').val() },
+        success: function (rs) {
+            showAlertFromResponse(rs);
+        }
+    });
+}
+
+
+function ConfigPaypal() {
+    msg_clear();
+
+    var mod = '';
+    if ($('#Mode-sanbox').prop('checked')) {
+        mod = 'sanbox';
+    }
+    else {
+        mod = 'live';
+    }
+    var conectionTimeout = $('#ConnectionTimeout').val();
+    var clientID = $('#ClientID').val();
+    var clientSecret = $('#ClientSecret').val();
+    var id = $('#ConfigPaypalId').val();
+
+    if (conectionTimeout.trim() == '') {
+        $('#error-timeout').show();
+        return;
+    }
+    else {
+        $('#error-timeout').hide();
+    }
+
+    if (clientID.trim() == '') {
+        $('#error-clientID').show();
+        return;
+    }
+    else {
+        $('#error-clientID').hide();
+    }
+
+    if (clientSecret == '') {
+        $('#error-ClientSecret').show();
+        return;
+    }
+    else {
+        $('error-ClientSecret').hide();
+    }
+
+    $.ajax({
+        url: UrlContent("/Admin/ConfigPaypal"),
+        type: 'POST',
+        data: { Id: id, Mode: mod, ConnectionTimeout: conectionTimeout, ClientId: clientID, ClientSecret: clientSecret },
+        success: function (rs) {
+            showAlertFromResponse(rs);
+        }
+    });
+}
+
+function ConfigEmail() {
+    msg_clear();
+
+    var id = $('#configEmailID').val();
+    var email = $('#ConfigEmail').val();
+    var displayName = $('#configDisplayName').val();
+    var host = $('#configHost').val();
+    var port = $('#configPort').val();
+    var userName = $('#configUserName').val();
+    var password = $('#configPassword').val();
+    var ssl = $('#ckSSL').prop('checked');
+    var credentials = $('#ckCredentials').prop('checked');
+
+    if (email.trim() == '') {
+        $('#error-ConfigEmail').show();
+        return;
+    }
+    else {
+        $('#error-ConfigEmail').hide();
+    }
+
+    if (displayName.trim() == '') {
+        $('#error-DisplayName').show();
+        return;
+    }
+    else {
+        $('#error-DisplayName').hide();
+    }
+
+    if (host.trim() == '') {
+        $('#error-configHost').show();
+        return;
+    }
+    else {
+        $('#error-configHost').hide();
+    }
+
+    if (port.trim() == '') {
+        $('#error-configPort').show();
+        return;
+    }
+    else {
+        $('#error-configPort').hide();
+    }
+
+    if (userName.trim() == '') {
+        $('#error-configUserName').show();
+        return;
+    }
+    else {
+        $('#error-configUserName').hide();
+    }
+
+    if (password.trim() == '') {
+        $('#error-configPassword').show();
+        return;
+    }
+    else {
+        $('#error-configPassword').hide();
+    }
+
+    $.ajax({
+        url: UrlContent("/Admin/ConfigEmail"),
+        type: 'POST',
+        data: { Id: id, Email: email, DisplayName: displayName, Host: host, Port: port, Username: userName, Password: password, EnableSsl: ssl, UseDefaultCredentials: credentials },
+        success: function (rs) {
+            showAlertFromResponse(rs);
+        }
+    });
+}
---
+++ SBS_Ecommerce/SBS_Ecommerce/Models/ViewModels/AdminViewModels.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SBS_Ecommerce.Models.ViewModels
+{
+    public class DeliveryCompanyViewModel
+    {
+        public int Id { get; set; }
+        public int CompanyId { get; set; }
+        [Required]
+        public string CompanyName { get; set; }
+        [Required]
+        public string Address { get; set; }
+        [Required]
+        public string Country { get; set; }
+        public string Ward { get; set; }
+        public string District { get; set; }
+        public string City { get; set; }
+        public string Phone { get; set; }
+        public string Fax { get; set; }
+        public string Email { get; set; }
+    }
+}
